a8b6e56bccf5afaf456ebbe60aa2433a862de2a8
hrgrafton92 <70491168+hrgrafton92@users.noreply.github.com>
2022-04-20 22:38:37 -0400

    Update CMPD_Traffic_Stops.py

--- CMPD_Traffic_Stops.py
+++ CMPD_Traffic_Stops.py
@@ -19,7 +19,7 @@ page = st_btn_select(
   # The different pages
   ('Home Page','Drivers', 'CMPD Divisions & Officers'),
   # Enable navbar
-  nav=True
+  nav=True,
 )
 
 # Display the right things according to the page
